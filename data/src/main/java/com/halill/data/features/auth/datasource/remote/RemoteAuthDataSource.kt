5c4c07317636120734b235db304897ba2eae64d4
Jaewon <turitus@naver.com>
2021-12-23 11:11:50 +0900

    [UPDATE] build file

--- data/src/main/java/com/halill/data/features/auth/datasource/remote/RemoteAuthDataSource.kt
+++ data/src/main/java/com/halill/data/features/auth/datasource/remote/RemoteAuthDataSource.kt
@@ -1,4 +1,11 @@
 package com.halill.data.features.auth.datasource.remote
 
-class RemoteAuthDataSource {
+import com.halill.data.features.auth.dto.response.LoginResponse
+import com.halill.domain.auth.parameter.LoginParameter
+import com.halill.domain.auth.parameter.RegisterParameter
+
+interface RemoteAuthDataSource {
+    suspend fun login(parameter: LoginParameter): LoginResponse
+
+    suspend fun register(parameter: RegisterParameter)
 }
--- data/src/main/java/com/halill/data/features/auth/repository/RegisterRepositoryImpl.kt
+++ data/src/main/java/com/halill/data/features/auth/repository/RegisterRepositoryImpl.kt
@@ -1,9 +1,15 @@
 package com.halill.data.features.auth.repository
 
+import com.halill.data.features.auth.datasource.local.LocalAuthDataSource
+import com.halill.data.features.auth.datasource.remote.RemoteAuthDataSource
 import com.halill.domain.auth.parameter.RegisterParameter
 import com.halill.domain.auth.repository.RegisterRepository
+import javax.inject.Inject
 
-class RegisterRepositoryImpl: RegisterRepository {
+class RegisterRepositoryImpl @Inject constructor(
+    private val remoteAuthDataSource: RemoteAuthDataSource,
+    private val localAuthDataSource: LocalAuthDataSource
+): RegisterRepository {
     override suspend fun register(parameter: RegisterParameter) {
         TODO("Not yet implemented")
     }

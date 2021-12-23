3468c914a715bba59ceaed6a898882c397eea9ad
Jaewon <turitus@naver.com>
2021-12-23 10:26:21 +0900

    [CREATE] register repository impl

---
+++ data/src/main/java/com/halill/data/features/auth/repository/RegisterRepositoryImpl.kt
@@ -0,0 +1,10 @@
+package com.halill.data.features.auth.repository
+
+import com.halill.domain.auth.parameter.RegisterParameter
+import com.halill.domain.auth.repository.RegisterRepository
+
+class RegisterRepositoryImpl: RegisterRepository {
+    override suspend fun register(parameter: RegisterParameter) {
+        TODO("Not yet implemented")
+    }
+}

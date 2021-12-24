f76d2ba491d6baaa21c8a12e84350953aea692ad
Jaewon <turitus@naver.com>
2021-12-24 14:42:24 +0900

    [UPDATE] not login exception

--- data/src/main/java/com/halill/data/features/todolist/repository/GetUserInfoRepositoryImpl.kt
+++ data/src/main/java/com/halill/data/features/todolist/repository/GetUserInfoRepositoryImpl.kt
@@ -1,10 +1,17 @@
 package com.halill.data.features.todolist.repository
 
+import com.halill.data.features.auth.datasource.remote.RemoteAuthDataSource
+import com.halill.data.local.LocalStorage
 import com.halill.domain.features.auth.entity.User
 import com.halill.domain.features.todolist.repository.GetUserInfoRepository
+import javax.inject.Inject
 
-class GetUserInfoRepositoryImpl: GetUserInfoRepository {
+class GetUserInfoRepositoryImpl @Inject constructor(
+    private val remoteAuthDataSource: RemoteAuthDataSource,
+    private val localStorage: LocalStorage
+): GetUserInfoRepository {
     override suspend fun getUserInfo(): User {
-        TODO("Not yet implemented")
+        val refreshToken = localStorage.getRefreshToken()
+        remoteAuthDataSource.refreshToken(refreshToken)
     }
 }

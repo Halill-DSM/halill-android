91e11458cf9a758a6488a878983a57ac312df6b1
Jaewon <turitus@naver.com>
2021-12-24 13:53:24 +0900

    [UPDATE] get user info use case

--- domain/src/main/java/com/halill/domain/features/todolist/usecase/GetUserInfoAndTodoListUseCase.kt
+++ domain/src/main/java/com/halill/domain/features/todolist/usecase/GetUserInfoAndTodoListUseCase.kt
@@ -3,12 +3,13 @@ package com.halill.domain.features.todolist.usecase
 import com.halill.domain.base.UseCase
 import com.halill.domain.features.todolist.entity.UserTodoList
 import com.halill.domain.features.todolist.repository.GetTodoListRepository
+import com.halill.domain.features.todolist.repository.GetUserInfoRepository
 import javax.inject.Inject
 
 class GetUserInfoAndTodoListUseCase @Inject constructor(
+    private val getUserInfoRepository: GetUserInfoRepository,
     private val getTodoListRepository: GetTodoListRepository
 ): UseCase<Unit, UserTodoList>() {
-    override suspend fun execute(data: Unit): UserTodoList {
-        TODO("Not yet implemented")
-    }
+    override suspend fun execute(data: Unit): UserTodoList =
+        UserTodoList(getUserInfoRepository.getUserInfo(), getTodoListRepository.getTodoList())
 }

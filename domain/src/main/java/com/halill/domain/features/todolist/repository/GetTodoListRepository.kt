91b377dfcf870da9be1a1b24df20fa2afa5a7f8b
Jaewon <turitus@naver.com>
2021-12-24 13:52:07 +0900

    [UPDATE] get todo list repository

--- domain/src/main/java/com/halill/domain/features/todolist/repository/GetTodoListRepository.kt
+++ domain/src/main/java/com/halill/domain/features/todolist/repository/GetTodoListRepository.kt
@@ -1,4 +1,7 @@
 package com.halill.domain.features.todolist.repository
 
+import com.halill.domain.features.todolist.entity.TodoModel
+
 interface GetTodoListRepository {
+    suspend fun getTodoList(): List<TodoModel>
 }

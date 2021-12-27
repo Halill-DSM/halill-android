8380d4ddea118fd0dd09483112ca8951d5e11662
Jaewon <turitus@naver.com>
2021-12-28 00:29:08 +0900

    [UPDATE] main activity

--- app/src/main/java/com/halill/halill/main/MainActivity.kt
+++ app/src/main/java/com/halill/halill/main/MainActivity.kt
@@ -5,9 +5,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import com.halill.halill.ui.theme.Halill_AndroidTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -18,24 +15,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Halill_AndroidTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    HalIllApp()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    Halill_AndroidTheme {
-        Greeting("Android")
     }
 }

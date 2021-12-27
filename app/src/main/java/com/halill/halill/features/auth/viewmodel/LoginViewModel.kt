d1ea0ffae4b184f4bffb6a85ffb91bb4a4b36f7e
Jaewon <turitus@naver.com>
2021-12-28 00:28:54 +0900

    [UPDATE] login view model

--- app/src/main/java/com/halill/halill/features/auth/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/halill/halill/features/auth/viewmodel/LoginViewModel.kt
@@ -1,4 +1,11 @@
 package com.halill.halill.features.auth.viewmodel
 
-class LoginViewModel {
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class LoginViewModel @Inject constructor(
+
+): ViewModel() {
 }

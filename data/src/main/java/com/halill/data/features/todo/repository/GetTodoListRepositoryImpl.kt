44bf29c712c4084eb86a9f5e2f34ca0f9ac9e9cc
Jaewon <turitus@naver.com>
2021-12-28 15:46:54 +0900

    [UPDATE] get todo list repository

--- data/src/main/java/com/halill/data/features/todo/repository/GetTodoListRepositoryImpl.kt
+++ data/src/main/java/com/halill/data/features/todo/repository/GetTodoListRepositoryImpl.kt
@@ -1,10 +1,15 @@
 package com.halill.data.features.todo.repository
 
+import com.halill.data.features.todo.datasource.local.LocalTodoDataSource
+import com.halill.data.features.todo.datasource.remote.RemoteTodoDataSource
 import com.halill.domain.features.todolist.entity.TodoModel
 import com.halill.domain.features.todolist.repository.GetTodoListRepository
 
-class GetTodoListRepositoryImpl: GetTodoListRepository {
+class GetTodoListRepositoryImpl(
+    private val localTodoDataSource: LocalTodoDataSource,
+    private val remoteTodoDataSource: RemoteTodoDataSource
+) : GetTodoListRepository {
     override suspend fun getTodoList(): List<TodoModel> {
-        TODO("Not yet implemented")
+
     }
 }

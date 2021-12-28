c762ee1c948b7c8ea301b026bcbe22e72b444c40
Jaewon <turitus@naver.com>
2021-12-28 19:21:49 +0900

    [UPDATE] auto data source di module

--- app/src/main/java/com/halill/halill/di/auth/AuthDataSourceModule.kt
+++ app/src/main/java/com/halill/halill/di/auth/AuthDataSourceModule.kt
@@ -2,6 +2,9 @@ package com.halill.halill.di.auth
 
 import com.halill.data.features.auth.datasource.local.LocalAuthDataSource
 import com.halill.data.features.auth.datasource.local.LocalAuthDataSourceImpl
+import com.halill.data.features.auth.datasource.remote.RemoteAuthDataSource
+import com.halill.data.features.auth.datasource.remote.RemoteAuthDataSourceImpl
+import com.halill.data.features.auth.remote.AuthApi
 import com.halill.data.local.LocalStorage
 import dagger.Module
 import dagger.Provides
@@ -15,4 +18,8 @@ object AuthDataSourceModule {
     @Singleton
     @Provides
     fun provideAuthLocalDataSource(localStorage: LocalStorage): LocalAuthDataSource = LocalAuthDataSourceImpl(localStorage)
+
+    @Singleton
+    @Provides
+    fun provideAuthRemoteDataSource(api: AuthApi): RemoteAuthDataSource = RemoteAuthDataSourceImpl(api)
 }

2d50e2c8ab908780e8aeaabcea6502a36666af76
Jaewon <turitus@naver.com>
2021-12-28 16:06:31 +0900

    [UPDATE] todo list repository

--- data/src/main/java/com/halill/data/features/todo/datasource/remote/RemoteTodoDataSource.kt
+++ data/src/main/java/com/halill/data/features/todo/datasource/remote/RemoteTodoDataSource.kt
@@ -1,4 +1,7 @@
 package com.halill.data.features.todo.datasource.remote
 
+import com.halill.domain.features.todolist.entity.TodoModel
+
 interface RemoteTodoDataSource {
+    suspend fun getTodoList(): List<TodoModel>
 }
--- data/src/main/java/com/halill/data/features/todo/repository/GetTodoListRepositoryImpl.kt
+++ data/src/main/java/com/halill/data/features/todo/repository/GetTodoListRepositoryImpl.kt
@@ -2,14 +2,19 @@ package com.halill.data.features.todo.repository
 
 import com.halill.data.features.todo.datasource.local.LocalTodoDataSource
 import com.halill.data.features.todo.datasource.remote.RemoteTodoDataSource
+import com.halill.data.util.OfflineCacheUtil
 import com.halill.domain.features.todolist.entity.TodoModel
 import com.halill.domain.features.todolist.repository.GetTodoListRepository
+import kotlinx.coroutines.flow.Flow
 
 class GetTodoListRepositoryImpl(
     private val localTodoDataSource: LocalTodoDataSource,
     private val remoteTodoDataSource: RemoteTodoDataSource
 ) : GetTodoListRepository {
-    override suspend fun getTodoList(): List<TodoModel> {
+    override suspend fun getTodoList(): Flow<List<TodoModel>> =
+        OfflineCacheUtil<List<TodoModel>>()
+            .localData { localTodoDataSource.getTodoList() }
+            .remoteData { remoteTodoDataSource.getTodoList() }
+            .createFlow()
 
-    }
 }

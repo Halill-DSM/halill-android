d58bfc750114f863a547e21f52ad601301294d11
Jaewon <turitus@naver.com>
2021-12-28 11:51:20 +0900

    [UPDATE] user data in state

--- domain/src/main/java/com/halill/domain/features/todolist/usecase/GetUserInfoAndTodoListUseCase.kt
+++ domain/src/main/java/com/halill/domain/features/todolist/usecase/GetUserInfoAndTodoListUseCase.kt
@@ -1,9 +1,11 @@
 package com.halill.domain.features.todolist.usecase
 
 import com.halill.domain.base.UseCase
+import com.halill.domain.features.todolist.doneList
 import com.halill.domain.features.todolist.entity.UserTodoList
 import com.halill.domain.features.todolist.repository.GetTodoListRepository
 import com.halill.domain.features.todolist.repository.GetUserInfoRepository
+import com.halill.domain.features.todolist.todoList
 import javax.inject.Inject
 
 class GetUserInfoAndTodoListUseCase @Inject constructor(
@@ -11,5 +13,5 @@ class GetUserInfoAndTodoListUseCase @Inject constructor(
     private val getTodoListRepository: GetTodoListRepository
 ): UseCase<Unit, UserTodoList>() {
     override suspend fun execute(data: Unit): UserTodoList =
-        UserTodoList(getUserInfoRepository.getUserInfo(), getTodoListRepository.getTodoList())
+        UserTodoList(getUserInfoRepository.getUserInfo(), getTodoListRepository.getTodoList().todoList(), getTodoListRepository.getTodoList().doneList())
 }

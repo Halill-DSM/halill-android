7d340bc051d92b93dcb477ee2cb9bb5136cebe53
Jaewon <turitus@naver.com>
2022-01-17 13:02:50 +0900

    [UPDATE] login button

--- app/src/main/java/com/halill/halill/features/auth/login/model/LoginState.kt
+++ app/src/main/java/com/halill/halill/features/auth/login/model/LoginState.kt
@@ -2,7 +2,7 @@ package com.halill.halill.features.auth.login.model
 
 sealed class LoginState {
     object NotDoneInputState : LoginState()
-    data class DoneInputState(val id: String, val password: String) : LoginState()
+    data class DoneInputState(val email: String, val password: String) : LoginState()
     object LoadingState : LoginState()
     object FinishState : LoginState()
     object WrongIdState : LoginState()
--- app/src/main/java/com/halill/halill/features/auth/login/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/halill/halill/features/auth/login/viewmodel/LoginViewModel.kt
@@ -1,7 +1,5 @@
 package com.halill.halill.features.auth.login.viewmodel
 
-import androidx.compose.runtime.mutableStateOf
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.halill.domain.exception.InternetErrorException
@@ -10,38 +8,59 @@ import com.halill.domain.features.auth.parameter.LoginParameter
 import com.halill.domain.features.auth.usecase.LoginUseCase
 import com.halill.halill.features.auth.login.model.LoginState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.single
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class LoginViewModel @Inject constructor(
     private val loginUseCase: LoginUseCase
-): ViewModel() {
-    val email = MutableLiveData<String>()
-    val password = MutableLiveData<String>()
+) : ViewModel() {
+    private val _email = MutableStateFlow("")
+    val email: StateFlow<String> = _email
 
-    val loginState = MutableLiveData<LoginState>()
+    private val _password = MutableStateFlow("")
+    val password: StateFlow<String> = _password
+
+    private val _loginState = MutableStateFlow<LoginState>(LoginState.NotDoneInputState)
+    val loginState: StateFlow<LoginState> = _loginState
 
     fun login() {
-        if(!email.value.isNullOrEmpty() && !password.value.isNullOrEmpty())
         viewModelScope.launch {
-            try {
-                loginUseCase.execute(LoginParameter(email.value!!, password.value!!))
-                loginState.value = LoginState.FinishState
-            } catch (e: WrongIdException) {
-                loginState.value = LoginState.WrongIdState
-            } catch (e: InternetErrorException) {
-                loginState.value = LoginState.InternetExceptionState
-            }
+            if (email.single().isNotEmpty() && password.single().isNotEmpty())
+
+                try {
+                    loginUseCase.execute(LoginParameter(email.single(), password.single()))
+                    _loginState.value = LoginState.FinishState
+                } catch (e: WrongIdException) {
+                    _loginState.value = LoginState.WrongIdState
+                } catch (e: InternetErrorException) {
+                    _loginState.value = LoginState.InternetExceptionState
+                }
         }
     }
 
     fun setEmail(email: String) {
-        this.email.value = email
+        _email.value = email
     }
 
     fun setPassword(password: String) {
-        this.password.value = password
+        viewModelScope.launch {
+            _password.emit(password)
+        }
     }
 
+    fun setDoneLoginState(email: String, password: String) {
+        viewModelScope.launch {
+            _loginState.emit(LoginState.DoneInputState(email, password))
+        }
+    }
+
+    fun setNotDoneInputState() {
+        viewModelScope.launch {
+            _loginState.emit(LoginState.NotDoneInputState)
+        }
+    }
 }

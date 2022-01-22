839c96190297c0ce45e4919787a934ec63e5d71e
Jaewon <turitus@naver.com>
2022-01-22 20:14:40 +0900

    [UPDATE] register back button

--- app/src/main/java/com/halill/halill/features/auth/register/Register.kt
+++ app/src/main/java/com/halill/halill/features/auth/register/Register.kt
@@ -1,29 +1,40 @@
 package com.halill.halill.features.auth.register
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.halill.halill.R
 import com.halill.halill.features.auth.IdTextField
 import com.halill.halill.features.auth.PasswordTextField
 import com.halill.halill.features.auth.register.viewmodel.RegisterViewModel
 
 @Composable
 fun Register(navController: NavController, viewModel: RegisterViewModel = hiltViewModel()) {
+    Image(painter = painterResource(id = R.drawable.ic_baseline_arrow_back_24),
+        contentDescription = "registerBack",
+        modifier = Modifier
+            .size(50.dp)
+            .padding(10.dp)
+            .clickable(enabled = true) {
+                navController.popBackStack()
+            }
+    )
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        val focusRequester = remember { FocusRequester() }
+
         val emailText = viewModel.email.collectAsState()
         val emailLabel = "이메일을 입력해주세요"
         IdTextField(
@@ -73,6 +84,7 @@ fun Register(navController: NavController, viewModel: RegisterViewModel = hiltVi
             keyboardType = KeyboardType.Text,
             imeAction = ImeAction.Done
         )
+        Spacer(modifier = Modifier.height(25.dp))
     }
 }
 

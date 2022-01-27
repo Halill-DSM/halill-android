24e670445e6ec4f45da6c9dd89bbea1f6b319004
Jaewon <turitus@naver.com>
2022-01-27 20:36:31 +0900

    [CREATE] todo repository

---
+++ data/src/main/java/com/halill/data/features/todo/repository/SaveTodoRepositoryImpl.kt
@@ -0,0 +1,14 @@
+package com.halill.data.features.todo.repository
+
+import com.halill.data.features.todo.database.dao.TodoDao
+import com.halill.domain.features.todo.entity.TodoEntity
+import com.halill.domain.features.todo.repository.SaveTodoRepository
+import javax.inject.Inject
+
+class SaveTodoRepositoryImpl @Inject constructor(
+    private val todoDao: TodoDao
+): SaveTodoRepository {
+    override suspend fun saveTodo(todo: TodoEntity) {
+        TODO("Not yet implemented")
+    }
+}

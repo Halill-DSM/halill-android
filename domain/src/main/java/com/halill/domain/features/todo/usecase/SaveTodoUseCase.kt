8f65cdebe5777ce23099c15a534818b68e216958
Jaewon <turitus@naver.com>
2022-01-27 21:14:15 +0900

    [CREATE] write todo file

--- domain/src/main/java/com/halill/domain/features/todo/usecase/SaveTodoUseCase.kt
+++ domain/src/main/java/com/halill/domain/features/todo/usecase/SaveTodoUseCase.kt
@@ -2,12 +2,13 @@ package com.halill.domain.features.todo.usecase
 
 import com.halill.domain.base.UseCase
 import com.halill.domain.features.todo.entity.TodoEntity
+import com.halill.domain.features.todo.repository.SaveTodoRepository
 import javax.inject.Inject
 
 class SaveTodoUseCase @Inject constructor(
-
+    private val saveTodoRepository: SaveTodoRepository
 ): UseCase<TodoEntity, Unit>() {
     override suspend fun execute(data: TodoEntity) {
-        TODO("Not yet implemented")
+        saveTodoRepository.saveTodo(data)
     }
 }

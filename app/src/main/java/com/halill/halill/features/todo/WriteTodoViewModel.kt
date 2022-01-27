2bb5f421b50df558bbbed78560c51d65be2e4169
Jaewon <turitus@naver.com>
2022-01-28 02:06:18 +0900

    [UPDATE] create write todo viewmodel

---
+++ app/src/main/java/com/halill/halill/features/todo/WriteTodoViewModel.kt
@@ -0,0 +1,9 @@
+package com.halill.halill.features.todo
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class WriteTodoViewModel: ViewModel() {
+
+}

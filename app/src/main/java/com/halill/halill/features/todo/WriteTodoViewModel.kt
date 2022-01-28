062ba49126a0c360801f9c3584480f30452b0ad0
Jaewon <turitus@naver.com>
2022-01-28 11:39:41 +0900

    [UPDATE] write todo viewmodel

--- app/src/main/java/com/halill/halill/features/todo/WriteTodoViewModel.kt
+++ app/src/main/java/com/halill/halill/features/todo/WriteTodoViewModel.kt
@@ -1,12 +1,46 @@
 package com.halill.halill.features.todo
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.halill.domain.features.todo.usecase.SaveTodoUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import java.time.LocalDateTime
 import javax.inject.Inject
 
 @HiltViewModel
 class WriteTodoViewModel @Inject constructor(
-
+    private val saveTodoUseCase: SaveTodoUseCase
 ): ViewModel() {
+
+    private val _title = MutableStateFlow("")
+    val title: StateFlow<String> = _title
+
+    private val _content = MutableStateFlow("")
+    val content: StateFlow<String> = _content
+
+    private val _deadLine = MutableStateFlow(LocalDateTime.now())
+    val deadLine: StateFlow<LocalDateTime> = _deadLine
+
+    fun setTitle(title: String) {
+        viewModelScope.launch {
+            _title.value = title
+        }
+    }
+
+    fun setContent(content: String) {
+        viewModelScope.launch {
+            _content.value = content
+        }
+    }
+
+    fun setDeadLine(deadLine: LocalDateTime) {
+        viewModelScope.launch {
+            _deadLine.value = deadLine
+        }
+    }
+
 
 }

4d368a1e4dfa7f52148fa8f19794ea3648f6f9c5
Jaewon <turitus@naver.com>
2022-01-28 20:00:07 +0900

    [UPDATE] check login repository

--- data/src/main/java/com/halill/data/features/todo/repository/GetUserInfoRepositoryImpl.kt
+++ data/src/main/java/com/halill/data/features/todo/repository/GetUserInfoRepositoryImpl.kt
@@ -1,31 +1,16 @@
 package com.halill.data.features.todo.repository
 
 import com.halill.data.features.auth.datasource.local.LocalAuthDataSource
-import com.halill.data.features.auth.datasource.remote.RemoteAuthDataSource
 import com.halill.data.features.auth.entity.toEntity
-import com.halill.domain.exception.NotLoginException
 import com.halill.domain.features.auth.entity.UserEntity
 import com.halill.domain.features.todo.repository.GetUserInfoRepository
-import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.collect
 import javax.inject.Inject
 
 class GetUserInfoRepositoryImpl @Inject constructor(
-    private val remoteAuthDataSource: RemoteAuthDataSource,
     private val localAuthDataSource: LocalAuthDataSource
 ) : GetUserInfoRepository {
-    override suspend fun getUserInfo(): Flow<UserEntity> {
-        val refreshTokenJob = Job()
-        CoroutineScope(Dispatchers.Default + refreshTokenJob).launch {
-            localAuthDataSource.getRefreshToken().collect { refreshToken ->
-                if (refreshToken.isNullOrEmpty()) {
-                    throw NotLoginException()
-                }
-                remoteAuthDataSource.refreshToken(refreshToken)
-            }
-        }
+    override suspend fun getUserInfo(): Flow<UserEntity> =
+        localAuthDataSource.getUser().toEntity()
 
-        return localAuthDataSource.getUser().toEntity()
-    }
 }

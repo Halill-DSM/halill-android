b45f67453ef89415c0a1e84bed8d46a91cc8fdfa
Jaewon <turitus@naver.com>
2022-01-28 11:16:03 +0900

    [UPDATE] write todo method

--- app/src/main/java/com/halill/halill/features/todo/WriteTodo.kt
+++ app/src/main/java/com/halill/halill/features/todo/WriteTodo.kt
@@ -1,9 +1,38 @@
 package com.halill.halill.features.todo
 
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.halill.halill.R
+import com.halill.halill.main.scaffoldState
+import com.halill.halill.ui.theme.Teal700
 
 @Composable
-fun WriteTodo(navController: NavController) {
+fun WriteTodo(navController: NavController, viewModel: WriteTodoViewModel = hiltViewModel()) {
+    Scaffold(scaffoldState = scaffoldState,
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = stringResource(id = R.string.write_todo))
+                },
+                navigationIcon = {
+                    IconButton(onClick = {
+                        navController.popBackStack()
+                    }) {
+                        Icon(Icons.Filled.ArrowBack, "back")
+                    }
+                },
+                backgroundColor = Teal700,
+                contentColor = Color.White,
+                elevation = 12.dp
+            )
+        }, content = {
 
+        })
 }
--- app/src/main/java/com/halill/halill/features/todo/WriteTodoViewModel.kt
+++ app/src/main/java/com/halill/halill/features/todo/WriteTodoViewModel.kt
@@ -2,8 +2,11 @@ package com.halill.halill.features.todo
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class WriteTodoViewModel: ViewModel() {
+class WriteTodoViewModel @Inject constructor(
+
+): ViewModel() {
 
 }

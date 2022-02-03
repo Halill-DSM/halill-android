608f47766cf6234855eaee6f29ef9dfc553cda20
Jaewon <turitus@naver.com>
2022-02-03 22:22:56 +0900

    [UPDATE] check done input

--- app/src/main/java/com/halill/halill/features/auth/login/model/LoginState.kt
+++ app/src/main/java/com/halill/halill/features/auth/login/model/LoginState.kt
@@ -4,5 +4,4 @@ sealed class LoginState {
     object NotDoneInputState : LoginState()
     data class DoneInputState(val email: String, val password: String) : LoginState()
     object LoadingState : LoginState()
-    object InternetExceptionState : LoginState()
 }

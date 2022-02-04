32fd79e4e8302ef1bd5dd0eae3cd1b9bd1b8eb76
Jaewon <turitus@naver.com>
2022-02-04 13:29:10 +0900

    [UPDATE] detail header bar

--- app/src/main/java/com/halill/halill/features/todo/detail/TodoDetail.kt
+++ app/src/main/java/com/halill/halill/features/todo/detail/TodoDetail.kt
@@ -1,10 +1,63 @@
 package com.halill.halill.features.todo.detail
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.halill.halill.R
+import com.halill.halill.main.scaffoldState
+import com.halill.halill.ui.theme.Teal700
 
 @Composable
-fun TodoDetail(navController: NavController, id: Long, viewModel: TodoDetailViewModel = hiltViewModel()) {
+fun TodoDetail(
+    navController: NavController,
+    id: Long,
+    viewModel: TodoDetailViewModel = hiltViewModel()
+) {
     viewModel.getDetail(id)
+    val state = viewModel.todoDetailState.collectAsState().value
+    Scaffold(scaffoldState = scaffoldState,
+        topBar = {
+            TopAppBar(
+                title = {
+                    if (state is TodoDetailState.MainState) {
+                        Text(text = state.todo.title)
+                    }
+                },
+                navigationIcon = {
+                    IconButton(onClick = {
+                        navController.popBackStack()
+                    }) {
+                        Icon(Icons.Filled.ArrowBack, "back")
+                    }
+                },
+                backgroundColor = Teal700,
+                contentColor = Color.White,
+                elevation = 12.dp
+            )
+        }, content = {
+            val scrollState = rememberScrollState()
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .verticalScroll(scrollState),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
+            }
+        })
 }

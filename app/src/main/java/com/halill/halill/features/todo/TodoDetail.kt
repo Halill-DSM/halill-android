1c18cc8b634ecab9300f391512df1e9b9c57bbfe
Jaewon <turitus@naver.com>
2022-02-04 12:35:36 +0900

    [UPDATE] todo detail nav

--- app/src/main/java/com/halill/halill/features/todo/TodoDetail.kt
+++ app/src/main/java/com/halill/halill/features/todo/TodoDetail.kt
@@ -1,8 +1,9 @@
 package com.halill.halill.features.todo
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
 
 @Composable
-fun TodoDetail() {
-    
+fun TodoDetail(navController: NavController) {
+
 }
--- app/src/main/java/com/halill/halill/main/HalillApp.kt
+++ app/src/main/java/com/halill/halill/main/HalillApp.kt
@@ -12,6 +12,7 @@ import androidx.navigation.compose.rememberNavController
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.halill.halill.features.auth.login.Login
 import com.halill.halill.features.auth.register.Register
+import com.halill.halill.features.todo.TodoDetail
 import com.halill.halill.features.todo.WriteTodo
 import com.halill.halill.ui.theme.HalIll_AndroidTheme
 import com.halill.halill.ui.theme.Teal900
@@ -42,6 +43,7 @@ fun HalIllApp() {
         composable("login") { Login(navController) }
         composable("register") { Register(navController) }
         composable("writeTodo") { WriteTodo(navController)}
+        composable("todoDetail") { TodoDetail(navController)}
     }
 }
 

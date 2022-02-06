4c8ef2e0dbc866f3e1d54f4ef04a87e0a4ff05c9
Jaewon <turitus@naver.com>
2022-02-06 22:15:07 +0900

    [UPDATE] filter method

--- domain/src/main/java/com/halill/domain/features/todo/entity/UserTodoListEntity.kt
+++ domain/src/main/java/com/halill/domain/features/todo/entity/UserTodoListEntity.kt
@@ -1,9 +1,12 @@
 package com.halill.domain.features.todo.entity
 
+import com.halill.domain.features.todo.doneList
+import com.halill.domain.features.todo.todoList
+
 data class UserTodoListEntity(val todoList: List<TodoEntity>, val doneList: List<TodoEntity>)
 
 fun List<TodoEntity>.toUserTodoListEntity() =
     UserTodoListEntity(
-        this.filter { !it.isCompleted },
-        this.filter { it.isCompleted }
+        this.todoList(),
+        this.doneList()
     )

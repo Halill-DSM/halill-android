67cf7c5ab6fec729ec46b0e318b0e505691f4cee
Jaewon <turitus@naver.com>
2022-02-21 04:54:20 +0900

    ⚰️ :: usecase module file

--- domain/src/main/java/com/halill/domain/features/auth/usecase/GetUserInfoUseCase.kt
+++ domain/src/main/java/com/halill/domain/features/auth/usecase/GetUserInfoUseCase.kt
@@ -4,8 +4,12 @@ import com.halill.domain.base.UseCase
 import com.halill.domain.features.auth.entity.UserEntity
 import com.halill.domain.features.auth.repository.GetUserInfoRepository
 import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
 
-class GetUserInfoUseCase(private val userInfoRepository: GetUserInfoRepository): UseCase<Unit, Flow<UserEntity>>() {
+class GetUserInfoUseCase @Inject constructor(
+    private val userInfoRepository: GetUserInfoRepository
+) : UseCase<Unit, Flow<UserEntity>>() {
+    
     override suspend fun execute(data: Unit): Flow<UserEntity> =
         userInfoRepository.getUserInfo()
 }

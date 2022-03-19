ce87daf826fd11c0294ac5c5190062ad4b0c1066
Jaewon <turitus@naver.com>
2022-03-19 12:01:56 +0900

    ⚡️:: save user Info

--- data/src/main/java/com/halill/data/features/auth/datasource/remote/RemoteRegisterDataSourceImpl.kt
+++ data/src/main/java/com/halill/data/features/auth/datasource/remote/RemoteRegisterDataSourceImpl.kt
@@ -1,21 +1,30 @@
 package com.halill.data.features.auth.datasource.remote
 
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.userProfileChangeRequest
 import com.halill.domain.features.auth.param.RegisterParam
 import javax.inject.Inject
 
 class RemoteRegisterDataSourceImpl @Inject constructor(
     private val auth: FirebaseAuth
-): RemoteRegisterDataSource {
+) : RemoteRegisterDataSource {
 
     override suspend fun register(registerParam: RegisterParam) {
         auth.createUserWithEmailAndPassword(registerParam.email, registerParam.password)
             .addOnCompleteListener { task ->
-                if(task.isSuccessful) {
-
-                } else {
-
+                if (task.isSuccessful) {
+                    setUserName(registerParam.name)
                 }
             }
+    }
+
+    private fun setUserName(userName: String) {
+        val user = auth.currentUser
+
+        val profileUpdates = userProfileChangeRequest {
+            displayName = userName
+        }
+
+        user!!.updateProfile(profileUpdates)
     }
 }

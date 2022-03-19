b93bcbb23cd9945584d04ea9c77aae113bec289a
Jaewon <turitus@naver.com>
2022-03-19 16:13:45 +0900

    ⚡️:: mypage view model

---
+++ app/src/main/java/com/halill/halill/features/mypage/MyPage.kt
@@ -0,0 +1,10 @@
+package com.halill.halill.features.mypage
+
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.halill.domain.features.auth.entity.UserEntity
+
+@Composable
+fun MyPage(userEntity: UserEntity, viewModel: MyPageViewModel = hiltViewModel()) {
+
+}
---
+++ app/src/main/java/com/halill/halill/features/mypage/MyPageViewModel.kt
@@ -0,0 +1,12 @@
+package com.halill.halill.features.mypage
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MyPageViewModel @Inject constructor(
+
+) : ViewModel() {
+
+}

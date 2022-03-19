ce52aa52fe62073a022c18536dc84719a62b24ff
Jaewon <turitus@naver.com>
2022-03-19 09:27:09 +0900

    ⚡️:: fetch user repository

--- data/src/main/java/com/halill/data/features/auth/repository/FetchUserInfoRepositoryImpl.kt
+++ data/src/main/java/com/halill/data/features/auth/repository/FetchUserInfoRepositoryImpl.kt
@@ -1,14 +1,14 @@
 package com.halill.data.features.auth.repository
 
+import com.halill.data.features.auth.datasource.local.LocalUserDataSource
 import com.halill.domain.features.auth.entity.UserEntity
 import com.halill.domain.features.auth.repository.FetchUserInfoRepository
 import javax.inject.Inject
 
 class FetchUserInfoRepositoryImpl @Inject constructor(
-
+    private val localUserDataSource: LocalUserDataSource
 ) : FetchUserInfoRepository {
 
-    override suspend fun fetchUserInfo(): UserEntity {
-        TODO("Not yet implemented")
-    }
+    override suspend fun fetchUserInfo(): UserEntity =
+        localUserDataSource.fetchUser()
 }

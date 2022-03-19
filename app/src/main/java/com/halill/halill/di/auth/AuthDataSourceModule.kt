a80d8ee6fa4fe1e1175e50dd302badd8500c0137
Jaewon <turitus@naver.com>
2022-03-19 09:56:58 +0900

    ⚡️:: login di

--- app/src/main/java/com/halill/halill/di/auth/AuthDataSourceModule.kt
+++ app/src/main/java/com/halill/halill/di/auth/AuthDataSourceModule.kt
@@ -3,6 +3,8 @@ package com.halill.halill.di.auth
 import com.google.firebase.auth.FirebaseAuth
 import com.halill.data.features.auth.datasource.local.LocalUserDataSource
 import com.halill.data.features.auth.datasource.local.LocalUserDataSourceImpl
+import com.halill.data.features.auth.datasource.remote.RemoteLoginDataSource
+import com.halill.data.features.auth.datasource.remote.RemoteLoginDataSourceImpl
 import com.halill.data.features.auth.datasource.remote.RemoteRegisterDataSource
 import com.halill.data.features.auth.datasource.remote.RemoteRegisterDataSourceImpl
 import dagger.Module
@@ -26,4 +28,10 @@ object AuthDataSourceModule {
     fun provideLocalUserDataSource(
         auth: FirebaseAuth
     ): LocalUserDataSource = LocalUserDataSourceImpl(auth)
+
+    @Singleton
+    @Provides
+    fun provideRemoteLoginDataSource(
+        auth: FirebaseAuth
+    ): RemoteLoginDataSource = RemoteLoginDataSourceImpl(auth)
 }

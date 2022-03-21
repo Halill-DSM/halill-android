e097964312434d49e38a7be0d983b5f0c3338b13
Jaewon <turitus@naver.com>
2022-03-21 22:36:14 +0900

    ⚰️ :: delete unusable method

--- data/src/main/java/com/halill/data/util/TaskCallBackUtil.kt
+++ data/src/main/java/com/halill/data/util/TaskCallBackUtil.kt
@@ -1,9 +1,7 @@
 package com.halill.data.util
 
-import android.util.Log
 import com.google.android.gms.tasks.Task
 import com.google.firebase.firestore.DocumentSnapshot
-import com.google.firebase.firestore.QuerySnapshot
 import com.halill.domain.exception.ReadFireBaseStoreFailException
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.channels.awaitClose
@@ -28,19 +26,6 @@ fun Task<DocumentSnapshot>.dataBaseTaskToFlow(): Flow<DocumentSnapshot> =
             trySendBlocking(it)
             close()
         }.addOnFailureListener {
-            throw ReadFireBaseStoreFailException()
-        }
-        awaitClose()
-    }
-
-@OptIn(ExperimentalCoroutinesApi::class)
-fun Task<QuerySnapshot>.dataBaseQueryTaskToFlow(): Flow<QuerySnapshot> =
-    callbackFlow {
-        this@dataBaseQueryTaskToFlow.addOnSuccessListener {
-            trySendBlocking(it)
-            close()
-        }.addOnFailureListener {
-            Log.e("queryError", it.toString())
             throw ReadFireBaseStoreFailException()
         }
         awaitClose()
